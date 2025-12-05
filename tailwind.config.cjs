588d8a7ee0a25ec115274fb53057c6669cc11e32
sotariye <ninabarango@gmail.com>
2025-12-04 19:07:49 -0500

    refactor: Externalize Tailwind CSS configuration and styles, and migrate to pnpm for package management.

---
+++ tailwind.config.cjs
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./index.html",
+        "./*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}",
+        "./services/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                brand: {
+                    orange: '#FD5F00',
+                    cream: '#FFEEE4',
+                    dark: '#190A00',
+                    brown: '#4C1D00',
+                    lightBrown: '#983900',
+                    peach: '#FE9F65'
+                }
+            },
+            fontFamily: {
+                display: ['Syne', 'sans-serif'],
+                body: ['Manrope', 'sans-serif'],
+            },
+            backgroundImage: {
+                'noise': "url('data:image/svg+xml,%3Csvg viewBox=%220 0 200 200%22 xmlns=%22http://www.w3.org/2000/svg%22%3E%3Cfilter id=%22noiseFilter%22%3E%3CfeTurbulence type=%22fractalNoise%22 baseFrequency=%220.65%22 numOctaves=%223%22 stitchTiles=%22stitch%22/%3E%3C/filter%3E%3Crect width=%22100%25%22 height=%22100%25%22 filter=%22url(%23noiseFilter)%22 opacity=%220.05%22/%3E%3C/svg%3E')",
+            }
+        }
+    },
+    plugins: [],
+}
